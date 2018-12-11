d56a5cbac4b79337c534082fce03511537d751a8
AlexanderSergan <jfdi686@gmail.com>
2018-12-11 17:22:26 +0200

    inited ohlc component

--- charts-demo/src/app/app.module.ts
+++ charts-demo/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OhlcComponent } from './components/charts/ohlc/ohlc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OhlcComponent
   ],
   imports: [
     BrowserModule,
