cc8aa97de7cbca8128429fb064592b579b7e80e2
AlexanderSergan <jfdi686@gmail.com>
2018-12-11 17:05:09 +0200

    initialized full stack project

--- charts-backend/src/main.ts
+++ charts-backend/src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+const port = process.env.PORT || 3000
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(port).then(
+    () => console.log(`Server lives at ${port} port.` ),
+    error => console.warn('Error occured: ', error),
+  )
 }
-bootstrap();
+bootstrap()
