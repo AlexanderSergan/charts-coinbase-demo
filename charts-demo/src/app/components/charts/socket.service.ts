a9226e0bbc9446ec70a262c65176d649679ef56f
AlexanderSergan <jfdi686@gmail.com>
2018-12-14 02:28:45 +0200

    Files linted

--- charts-demo/src/app/components/charts/socket.service.ts
+++ charts-demo/src/app/components/charts/socket.service.ts
@@ -4,42 +4,42 @@ import { Observable } from 'rxjs'
 // import { Message } from '../model/message';
 // import { Event } from '../model/event';
 
-import * as socketIo from 'socket.io-client';
+import * as socketIo from 'socket.io-client'
 
 const SERVER_URL = 'wss://ws-feed.pro.coinbase.com'
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SocketService implements OnInit {
 
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    // Add 'implements OnInit' to the class.
     // this.onMessage.
   }
 
   constructor() { }
-  private socket;
+  private socket
 
   public initSocket(): void {
       this.socket = socketIo(SERVER_URL)
   }
 
   public send(message: any): void {
-      this.socket.emit('message', message);
+      this.socket.emit('message', message)
   }
 
   public onMessage(): Observable<any> {
       return new Observable<any>(observer => {
-          this.socket.on('message', (data: any) => observer.next(data));
-      });
+          this.socket.on('message', (data: any) => observer.next(data))
+      })
   }
 
   public onEvent(event: Event): Observable<any> {
       return new Observable<Event>(observer => {
-          this.socket.on(event, () => observer.next());
-      });
+          this.socket.on(event, () => observer.next())
+      })
   }
 
 }
